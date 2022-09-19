b38b8795784f4a78fbe526860a6fc7f31eb6ff01
Christopher K Erlendson <christopherkerlendson@Christophers-MacBook-Pro.local>
2022-09-19 13:43:14 -0400

    commit tree 933a455091a57dd2fcd10ed71faa507bd663f6b3

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet) {
+  // gives customers a free sample if the ride is less than or equal to 400 feet
+  // if
+  // less than or equal
+  // 400
+  // console.log("This one is on me!");
+  // => undefined
+  5 * 5 + 5 + 5 * 5;
+  // => 25 + 5 + 25
+  // => 55
+  let x = feet;
+  if (x <= 400) {
+    return console.log("This one is on me!");
+  }
+}
+
+scuberGreetingForFeet(199);
+
+function ternaryCheckCity() {
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip() {
   // Write your code here!
 }
